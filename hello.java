694e91756145176833e5f2a22a1da5d1f74036cf
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-15 17:07:51 +0530

    .

--- hello.java
+++ hello.java
@@ -1,5 +1,22 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // This is a single line comment  
+        /*
+            Multiline comment 
+         */
+        // int a=20;
+        // char aa='k';
+        // System.out.println(aa);
+
+        int a=20;
+        int b=30;
+        System.out.println(a+b);
+        String s="karan";
+        String s1=new String("Vishal");
+        System.out.println(s1);
+        System.out.println(s);
+        
+
     }
 }
