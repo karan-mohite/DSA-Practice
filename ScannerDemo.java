0fb882c2efc4ce88c7a484b00d1f564bf0f00c26
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-15 17:37:34 +0530

    .

--- ScannerDemo.java
+++ ScannerDemo.java
@@ -4,12 +4,12 @@ public class ScannerDemo {
     public static void main(String[] args) {
         System.out.println("Enter Your Name:");
         Scanner sc = new Scanner(System.in);
-        String name=sc.nextLine();
+        String name = sc.nextLine();
         System.out.println("Enter Your Age:");
         Scanner sc1 = new Scanner(System.in);
-        int age=sc1.nextInt();
+        int age = sc1.nextInt();
 
-        System.out.println("Name of Studend is:" + name);
+        System.out.println("Name of Student is:" + name);
         System.out.println("Age of Student :" + age);
     }
 }
