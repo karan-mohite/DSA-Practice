2a10398d7abddbbf8e0c4bd31335ac8aad354d20
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-16 22:25:28 +0530

    .

---
+++ DisplaySquareofNumberForAnArray.java
@@ -0,0 +1,13 @@
+// From an arrya display the perfect number.
+public class DisplaySquareofNumberForAnArray {
+    public static void main(String[] args) {
+        // int arr[]={25,9,13,6,2};
+        // for(int i=0;i<arr.length;i++)
+        // {
+        // System.out.println(arr[i]*arr[i]);
+        // }
+
+        int n = (int) (Math.sqrt(13));
+        System.out.println(n);
+    }
+}
