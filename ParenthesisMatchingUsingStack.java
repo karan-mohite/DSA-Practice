d88c1d70e811de6ef3e1bd185551e0e50ef08386
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-21 00:50:44 +0530

    .

---
+++ ParenthesisMatchingUsingStack.java
@@ -0,0 +1,56 @@
+import java.util.Stack;
+
+public class ParenthesisMatchingUsingStack {
+    public static void main(String[] args) {
+        String expression = "{[()]}";
+        if(expression.length()%2==0)
+        {
+            boolean result = checkValidness(expression);
+
+            if (result == true) {
+                System.out.print("Well formed..!!");
+            } else {
+                System.out.print("Not well formed");
+            }
+        }
+        else
+        {
+            System.out.print("Not well formed..!!");
+        }
+       
+    }
+
+    private static boolean checkValidness(String expression) {
+
+        Stack<Character> stack = new Stack();
+
+        for (char ch : expression.toCharArray()) {
+            if (ch == '{' || ch == '(' || ch == '[') {
+                stack.push(ch);
+                continue;
+            }
+            if(stack.isEmpty())
+            {
+                return false;
+            }
+            char z;
+            switch (ch) {
+                case '}':
+                    z = stack.pop();
+                    if (z != '{')
+                        return false;
+                    break;
+                case ']':
+                    z = stack.pop();
+                    if (z != '[')
+                        return false;
+                    break;
+                case ')':
+                    z = stack.pop();
+                    if (z != '(')
+                        return false;
+            }
+        }
+        return (stack.isEmpty());
+    }
+}
