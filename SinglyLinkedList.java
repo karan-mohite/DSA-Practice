e1fd6470107099102eec10db73ec3031e496004f
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-28 20:31:39 +0530

    .

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -4,7 +4,41 @@ public class SinglyLinkedList {
     public static void main(String[] args) {
         Node head = null;
         head = create(head);
+        System.out.println("Display After Creation:");
         display(head);
+
+        head = addFirst(head);
+        System.out.println("Display After Adding at first position:");
+        display(head);
+
+        addLast(head);
+        System.out.println("Display After Adding at Last position:");
+        display(head);
+
+    }
+
+    private static Node addLast(Node head) {
+        Node temp = head;
+        while (temp.next != null)
+            temp = temp.next;
+        Node nn = getNode();
+        return temp.next = nn;
+
+    }
+
+    private static Node addFirst(Node head) {
+        Node nn = getNode();
+        nn.next = head;
+        head = nn;
+        return head;
+    }
+
+    private static Node getNode() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter data for Node:");
+        int info = sc.nextInt();
+        Node newNode = new Node(info);
+        return newNode;
     }
 
     private static void display(Node head) {
@@ -13,7 +47,7 @@ public class SinglyLinkedList {
             System.out.print(temp.data + "->");
             temp = temp.next;
         }
-        System.out.print("NULL");
+        System.out.print("NULL\n");
     }
 
     private static Node create(Node head) {
